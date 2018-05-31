31f33edc56d5f6e7f9d0672d734d47957e998a43
Jim Lin <jimlintw922@gmail.com>
2018-05-31 13:02:02 +0800

    mod mk-cert-key.sh

--- scripts/openssl/mk-cert-key.sh
+++ scripts/openssl/mk-cert-key.sh
@@ -30,4 +30,9 @@ OUTPUT_CERT=$OUTPUT_DIR/$CN.cert.pem
 [ -d $OUTPUT_DIR ] || mkdir $OUTPUT_DIR
 echo subjectAltName = IP.1:127.0.0.1 > $OUTPUT_EXT
 openssl req -new -sha256 -keyout $OUTPUT_KEY -out $OUTPUT_CSR -days 365 -newkey rsa:2048 -nodes -subj "$subj"
-openssl x509 -req -days 365 -sha1 -CAcreateserial -CA $CA_CERT -CAkey $CA_KEY -in $OUTPUT_CSR -extfile $OUTPUT_EXT -out $OUTPUT_CERT
+
+if [ -f .srl ]; then
+    openssl x509 -req -days 365 -sha1 -CAserial .srl -CA $CA_CERT -CAkey $CA_KEY -in $OUTPUT_CSR -extfile $OUTPUT_EXT -out $OUTPUT_CERT
+else
+    openssl x509 -req -days 365 -sha1 -CAcreateserial -CA $CA_CERT -CAkey $CA_KEY -in $OUTPUT_CSR -extfile $OUTPUT_EXT -out $OUTPUT_CERT
+fi
